7002ca9e611412ce6fb4b10cb04fe9c6c17f9df5
Satyam Mishra <YATRAONLINE\satyam.mishra2@lap0003682.yatraonline.local>
2026-06-06 18:23:58 +0530

    add posthog and setup of MongoDB

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,28 @@
+'use client';
+
 import Link from "next/link";
 import Image from "next/image";
 
 import React from 'react'
+import posthog from "posthog-js";
 
 const Navbar = () => {
+  const handleNavClick = (label: string, href: string) => {
+    posthog.capture('nav_link_clicked', { label, href });
+  };
+
   return (
     <header>
         <nav>
-            <Link href='/' className= "logo" >
+            <Link href='/' className= "logo" onClick={() => handleNavClick('DevEvent', '/')}>
               <Image src="/icons/logo.png" alt="nav" width={24} height={24}/>
               <p>DevEvent</p>
             </Link>
 
           <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/Events">Events</Link></li>
-            <li><Link href="/Create Event">Create Event</Link></li>
+            <li><Link href="/" onClick={() => handleNavClick('Home', '/')}>Home</Link></li>
+            <li><Link href="/Events" onClick={() => handleNavClick('Events', '/Events')}>Events</Link></li>
+            <li><Link href="/Create Event" onClick={() => handleNavClick('Create Event', '/Create Event')}>Create Event</Link></li>
           </ul>
 
         </nav>
